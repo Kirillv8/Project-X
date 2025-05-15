72d726cadbe66d7bec3d5b85cd13d55e98587398
Kirill <urmancheev.it@gmail.com>
2025-05-16 02:44:30 +0300

    feat: add small evolutionary design project structure
    - created 1-init/project-structure/ folder in src
    - added base folders: app, features, shared
    - app: App.tsx, main.tsx, index.css (global styles)
    - features:
      - auth: AuthPage, RegistrationPage
      - board: BoardPage
      - board-list: BoardListPage
      - header: AppHeaderPage

---
+++ src/1-init/project-structure/feature/board-list/index.tsx
@@ -0,0 +1,7 @@
+export const BoardsListPage = () => {
+  return (
+    <>
+      <div>Boards List Page</div>;
+    </>
+  );
+};
---
+++ src/1-init/project-structure/feature/header/index.tsx
@@ -0,0 +1,7 @@
+export const AppHeader = () => {
+  return (
+    <header>
+      <h1>My App Header</h1>
+    </header>
+  );
+};
